482a7c1365442e69e7a90ac0f1100d11b94310f6
Krzysiek <kr1sss@wp.pl>
2017-08-16 22:55:35 +0200

    changes and stylize

--- scripts.js
+++ scripts.js
@@ -1,28 +1,25 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var quoteUrl = "https://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
 
 function getQuote() {
     $.getJSON(quoteUrl, createTweet);
-};
+}
 
 function createTweet(input) {
     var data = input[0];
 
-    var quoteText = $(data.content).text().trim();
-    var quoteAuthor = data.title;
-
-    if (!quoteAuthor.length) {
-        quoteAuthor = "Unknown author";
+    if (!input.quoteAuthor.length) {
+        input.quoteAuthor = "Unknown author";
     }
 
-    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+    var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
 
     if (tweetText.length > 140) {
     	getQuote();
 	} else {
 		var tweet = tweetLink + encodeURIComponent(tweetText);
-    	$('.quote').text(quoteText);
-    	$('.author').text("Author: " + quoteAuthor);
+    	$('.quote').text(input.quoteText);
+    	$('.author').text("Author: " + input.quoteAuthor);
     	$('.tweet').attr('href', tweet);
 	}
 }
